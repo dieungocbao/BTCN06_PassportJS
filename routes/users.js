bce38331e413c24ea2528aba33ade2ef9e7266d5
dieungocbao <dieungocbao@gmail.com>
2019-10-16 21:12:04 +0700

    resgister

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const User = require('../models/User')
+const { registerValidation, loginValidation } = require('../validation')
+const bcrypt = require('bcryptjs')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register', async function(req, res, next) {
+  const { error } = registerValidation(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
-module.exports = router;
+  const { name, email, password } = req.body
+
+  const emailExist = await User.findOne({ email })
+  if (emailExist) return res.status(400).send('Email already exists')
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashPassword = await bcrypt.hash(password, salt)
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashPassword
+  })
+  try {
+    const savedUser = await newUser.save()
+    res.send(savedUser)
+  } catch (err) {
+    return res.status(400).send(err)
+  }
+})
+
+router.post('/login', async function(req, res, next) {d
+  const { error } = loginValidation(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const { email, password } = req.body
+
+
+})
+
+module.exports = router
